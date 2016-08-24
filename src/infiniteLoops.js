312ff60ff0a698b41b2f2a8744854ca9316534b1
Daesy Stephens <daesyramos@hotmail.com>
2016-08-23 19:46:07 -0700

    Completed 2nd problem

--- src/infiniteLoops.js
+++ src/infiniteLoops.js
@@ -21,20 +21,32 @@ var testVowel = function(char){
 
 // 1. countVowels
 var countVowels = function(myString){
-	var vowelCounter = 0
-	var arry=myString.split();
+	var vowelCounter = 0;
+	var arry=myString.split('');
+	console.log(arry);
 	loop(arry, function(letter){
-		if (testVowel(letter) === true){
-			vowelCounter = vowelCounter+1;
-	};
+		if (testVowel(letter)){
+			vowelCounter++ ;
+	};	
+});
 	console.log(vowelCounter);
 	return vowelCounter;
-});
-}
+};
 console.log(countVowels('apples'));
+
 // 2. stringReversal
+var stringReversal = function (myString){
+	var splitArray = myString.split('');
+	var reverseArray = [];
+	loop(splitArray, function(letter){
+		reverseArray.unshift(letter);
+	});
+	return reverseArray.join("");
+};
+console.log (stringReversal('apples'));
 
 // 3. isPalindrome
+
 
 // 4. largestNumber
 
